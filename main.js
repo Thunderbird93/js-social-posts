e41d81d990595b6692df4a088599ed619082cc52
Thunderbird93 <antircristian@gmail.com>
2022-10-27 16:42:41 +0200

    [add] all data in litteral

--- main.js
+++ main.js
@@ -67,28 +67,28 @@ function addPosts(){
         <div class="post__header">
         <div class="post-meta">                    
         <div class="post-meta__icon">
-        <img class="profile-pic" src="${post}" alt="${post}">                    
+        <img class="profile-pic" src="${post.author.image}" alt="${post.author}">                    
         </div>
         <div class="post-meta__data">
-        <div class="post-meta__author">${post}</div>
+        <div class="post-meta__author">${post.author.name}</div>
         <div class="post-meta__time">${post.created}</div>
         </div>                    
         </div>
         </div>
         <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
         <div class="post__image">
-        <img src="${post}" alt="${post}">
+        <img src="${post.media}" alt="${post.image}">
         </div>
         <div class="post__footer">
-        <div class="likes js-likes" onclick="checkLike(${post})">
+        <div class="likes js-likes" onclick="checkLike(${post.id})">
         <div class="likes__cta">
-        <a class="like-button  js-like-button" href="#" data-postid="${post}">
+        <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
         <span class="like-button__label">Mi Piace</span>
         </a>
         </div>
         <div class="likes__counter">
-        Piace a <b id="like-counter-1" class="js-likes-counter">${post}</b> persone
+        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
         </div>
         </div> 
         </div>            
