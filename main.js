8a9c0e5e940c88a04b9f301d9727cb02c5a43d7e
Thunderbird93 <antircristian@gmail.com>
2022-10-27 16:10:40 +0200

    [add] collegamento post

--- main.js
+++ main.js
@@ -67,28 +67,28 @@ function addPosts(){
         <div class="post__header">
         <div class="post-meta">                    
         <div class="post-meta__icon">
-        <img class="profile-pic" src="${-}" alt="${-}">                    
+        <img class="profile-pic" src="${post}" alt="${post}">                    
         </div>
         <div class="post-meta__data">
-        <div class="post-meta__author">${-}</div>
-        <div class="post-meta__time">${-}</div>
+        <div class="post-meta__author">${post}</div>
+        <div class="post-meta__time">${post}</div>
         </div>                    
         </div>
         </div>
         <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
         <div class="post__image">
-        <img src="${-}" alt="${-}">
+        <img src="${post}" alt="${post}">
         </div>
         <div class="post__footer">
-        <div class="likes js-likes" onclick="checkLike(${-})">
+        <div class="likes js-likes" onclick="checkLike(${post})">
         <div class="likes__cta">
-        <a class="like-button  js-like-button" href="#" data-postid="${-}">
+        <a class="like-button  js-like-button" href="#" data-postid="${post}">
         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
         <span class="like-button__label">Mi Piace</span>
         </a>
         </div>
         <div class="likes__counter">
-        Piace a <b id="like-counter-1" class="js-likes-counter">${-}</b> persone
+        Piace a <b id="like-counter-1" class="js-likes-counter">${post}</b> persone
         </div>
         </div> 
         </div>            
