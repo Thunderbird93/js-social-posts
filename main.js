ce96793d7a1e913a578ab5cbd4f9240b49080e1b
Thunderbird93 <antircristian@gmail.com>
2022-10-27 16:07:55 +0200

    [add] litteral

--- main.js
+++ main.js
@@ -60,7 +60,43 @@ const posts = [
 const container = document.getElementById('container');
 
 function addPosts(){
+    let x = '';
+    posts.forEach(post => {
+        x += `
+        <div class="post">
+        <div class="post__header">
+        <div class="post-meta">                    
+        <div class="post-meta__icon">
+        <img class="profile-pic" src="${-}" alt="${-}">                    
+        </div>
+        <div class="post-meta__data">
+        <div class="post-meta__author">${-}</div>
+        <div class="post-meta__time">${-}</div>
+        </div>                    
+        </div>
+        </div>
+        <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+        <div class="post__image">
+        <img src="${-}" alt="${-}">
+        </div>
+        <div class="post__footer">
+        <div class="likes js-likes" onclick="checkLike(${-})">
+        <div class="likes__cta">
+        <a class="like-button  js-like-button" href="#" data-postid="${-}">
+        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        <span class="like-button__label">Mi Piace</span>
+        </a>
+        </div>
+        <div class="likes__counter">
+        Piace a <b id="like-counter-1" class="js-likes-counter">${-}</b> persone
+        </div>
+        </div> 
+        </div>            
+        </div>
+        `
     })
+    
+    container.innerHTML = x;
 }
 
 addPosts();
